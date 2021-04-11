a80335794a414030edac48b724b0053a597b2ce1
UnlimitedZone <lli142@ucsc.edu>
2021-04-12 05:40:55 +0800

    Complete

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -6,8 +6,11 @@ class Rocket extends Phaser.GameObjects.Sprite{
         //add object to the existing scene
         scene.add.existing(this);
 
-        this.isFiring = false;
+        this.isFiring = false;      //track rocket firing status
         this.moveSpeed = 2;
+        this.firingSpeed = 2;
+
+        this.sfxRocket = scene.sound.add('sfx_rocket'); // add rocket sfx
     }
 
     update(){
@@ -19,5 +22,29 @@ class Rocket extends Phaser.GameObjects.Sprite{
                 this.x += this.moveSpeed;
             }
         }
+
+        if(Phaser.Input.Keyboard.JustDown(keyF)){
+            this.isFiring = true;
+            
+            this.sfxRocket.play();  // play sfx
+        }
+
+        //if fired, move the rocket up
+        if(this.isFiring && this.y >= borderUISize * 3 + borderPadding ){
+            this.y -= this.firingSpeed;
+            
+        }
+
+        //reset on miss
+        if(this.y <= borderUISize *3 + borderPadding){
+            this.reset();
+        }
+    }
+
+
+    // reset rocket to "ground"
+    reset(){
+        this.isFiring = false;
+        this.y = game.config.height - borderUISize - borderPadding;
     }
 }
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -3,11 +3,61 @@ class Menu extends Phaser.Scene {
         super("menuScene");
     }
 
+    preload() {
+        // load audio
+        this.load.audio('sfx_select', './assets/blip_select12.wav');
+        this.load.audio('sfx_explosion', './assets/explosion38.wav');
+        this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
+    }
 
     create(){
-        this.add.text(20, 20, "Rocket Patrol Menu");
+        //this.scene.start("PlayScene");
+        ///this.add.text(20, 20, "Rocket Patrol Menu");
+
+        // menu
+        let menuConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#F3B141',
+            color: '#843605',
+            align: 'right',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 0
+        }
 
         //change scenes
-        this.scene.start("PlayScene");
+        
+
+        // define keys
+        this.add.text(game.config.width/2, game.config.height/2 - 64, 'Rocket Patrol Menu', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2, 'Use <--> arrows to move & (F) to fire', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press <- for Novice or -> for Expert', menuConfig).setOrigin(0.5);
+
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
     }
+
+    update() {
+        if (Phaser.Input.Keyboard.JustDown(keyLEFT)) {
+          // easy mode
+          game.settings = {
+            spaceshipSpeed: 3,
+            gameTimer: 5000    
+          }
+          this.sound.play('sfx_select');
+          this.scene.start('PlayScene');    
+        }
+        if (Phaser.Input.Keyboard.JustDown(keyRIGHT)) {
+          // hard mode
+          game.settings = {
+            spaceshipSpeed: 4,
+            gameTimer: 45000    
+          }
+          this.sound.play('sfx_select');
+          this.scene.start('PlayScene');    
+        }
+      }
 }
